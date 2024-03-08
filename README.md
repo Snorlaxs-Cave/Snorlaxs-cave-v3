72d0721b4542939ec6c37e0ff0a1da474fa4c9a9
snorlax <143361136+snorlaxscave@users.noreply.github.com>
2024-03-08 09:23:14 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+<div align="center">
+  
+# Welcome to Snorlax's Cave!
+Snorlax's Cave is a UV built proxy and game site **(v2.0 Release)**
+</div>
+
+## We offer:
+- Many Unblocked Apps & Games
+- Built in web proxy
+- Free emulator
+- Many customizable features
+
+## Join our community!
+<a align="center" href="https://discord.gg/Cqaa8x82Ch">
+    <img src="https://invidget.switchblade.xyz/Cqaa8x82Ch?theme=dark"/>
+</a>
+
+## Local Hosting
+
+```bash
+git clone https://github.com/SPS-Services/Snorlaxs-Cave-v1.git
+```
+```bash
+cd snorlaxs cave
+```
+```bash
+node index.js
+```
+
+
+## Hosting
+**Warning: You will be unable to edit Ultraviolet due to the ban of proxies on Replit, if this is an issue, you should set up using a different method!**<br/>
+[![Run on Replit](https://binbashbanana.github.io/deploy-buttons/buttons/remade/replit.svg)](https://replit.com/github/SPS-Services/Snorlaxs-Cave)
+[![Remix on Glitch](https://binbashbanana.github.io/deploy-buttons/buttons/remade/glitch.svg)](https://glitch.com/edit/#!/import/github/SPS-Services/Snorlaxs-Cave)
+[![Deploy on Railway](https://binbashbanana.github.io/deploy-buttons/buttons/remade/railway.svg)](https://railway.app/new/template?template=https://github.com/SPS-Services/Snorlaxs-Cave)
+[![Deploy to Cyclic](https://binbashbanana.github.io/deploy-buttons/buttons/remade/cyclic.svg)](https://app.cyclic.sh/api/app/deploy/Snorlaxs-Cave)
+[![Deploy to Koyeb](https://binbashbanana.github.io/deploy-buttons/buttons/remade/koyeb.svg)](https://app.koyeb.com/deploy?type=git&repository=github.com/SPS-Services/Snorlaxs-Cave&branch=main&name=Snorlaxs-Cave)
+[![Deploy to Render](https://binbashbanana.github.io/deploy-buttons/buttons/remade/render.svg)](https://render.com/deploy?repo=https://github.com/SPS-Services/Snorlaxs-Cave)
+[![Deploy with Vercel](https://binbashbanana.github.io/deploy-buttons/buttons/remade/vercel.svg)](https://vercel.com/new/clone?repositoryurl=https://github.com/SPS-Services/Snorlaxs-Cave)
+## **[SUPPORT US!](https://patreon.com/SnorlaxCave)**
---
+++ index.js
@@ -0,0 +1,90 @@
+import express from "express";
+import http from "node:http";
+import createBareServer from "@tomphttp/bare-server-node";
+import path from "node:path";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+app.get("/math", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "Games.html"));
+});
+app.get("/english", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "Apps.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "About.html"));
+});
+app.get("/settings", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "Settings.html"));
+});
+app.get("/portal", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "loader.html"));
+});
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "agloader.html"));
+});
+app.get("/snorlax", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/people-secrets/", "snorlax.html"));
+});
+app.get("/tlochsta", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/people-secrets/", "tlochsta.html"));
+});
+app.get("/fowntain", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/people-secrets/", "fowntain.html"));
+});
+app.get("/bigfoot", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/people-secrets/", "bigfoot.html"));
+});
+app.get("/burb", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/people-secrets/", "burb.html"));
+});
+app.get("/derpman", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/people-secrets/", "derpman.html"));
+});
+app.get("/cats", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/people-secrets/", "cats.html"));
+});
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "404.html"));
+});
+
+server.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on("listening", () => {
+  console.log(`Snorlax's Cave listening on port ${process.env.PORT}`);
+});
+
+server.listen({
+  port: process.env.PORT,
+});
