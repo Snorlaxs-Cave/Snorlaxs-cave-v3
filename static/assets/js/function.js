88953911cb9de3ca42a68a9d41ab9a847cb72c0f
snorlax <143361136+snorlaxscave@users.noreply.github.com>
2024-04-19 13:41:04 +0000

    Update function.js

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -12,7 +12,7 @@ function openLink(url) {
 }
 
 function ourDiscord() {
-    window.location.href = 'https://discord.gg/Cqaa8x82Ch';
+    window.location.href = 'https://discord.gg/snorlaxscave';
 }
 
 
@@ -51,4 +51,3 @@ function cloak() {
     w.document.write('<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + url + '"></iframe>');
     window.close('', '_parent', '');
 }
-
